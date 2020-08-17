049fff89e4ef81ed1be06ec48a8d91d6c59ef8ee
wasdjkl <wasd_jkl@outlook.com>
2020-08-17 16:23:36 +0800

    feat: Update docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -32,6 +32,6 @@ docker run hello-world
 
 # Install Compose
 echo -e "\033[33m Install Compose \033[0m"
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
